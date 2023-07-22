5b8e5b8802a1a1f91fd1ccd0081cfc6329d8ada9
azhhng <azhhng@gmail.com>
2023-07-22 01:05:11 -0400

    add dynamic loading of survey questions

---
+++ src/components/SurveyForm/SurveyForm.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import "./SurveyForm.css";
+import {
+  YesNoSelections,
+  AppliedSelections,
+} from "../../enums/questionSelections";
+
+function SurveyForm(props) {
+  const [questions, setQuestions] = useState([]);
+  const [responses, setResponses] = useState({});
+
+  useEffect(() => {
+    const questionsArray = [];
+    for (const question in props.questions) {
+      const questionObject = props.questions[question];
+      switch (questionObject["selectionType"]) {
+        case "dropdown":
+          // set responses to the question
+          let selectionArray;
+          if (questionObject.selections === "YesNoSelections") {
+            selectionArray = YesNoSelections;
+          } else if (questionObject.selections === "AppliedSelections") {
+            selectionArray = AppliedSelections;
+          } else {
+            selectionArray = questionObject.selections;
+          }
+          const selectionOptions = selectionArray.map((selection) => {
+            return (
+              <option key={selection} value={selection}>
+                {selection}
+              </option>
+            );
+          });
+          // add question to the array
+          questionsArray.push(
+            <div className="question-container" key={questionObject["id"]}>
+              <label className="question-label">{question}</label>
+              <select
+                key={questionObject["id"]}
+                id={questionObject["id"]}
+                onChange={(event) =>
+                  setResponses(...responses, { question: event.target.value })
+                }
+              >
+                {selectionOptions}
+              </select>
+            </div>
+          );
+          break;
+        case "integer":
+          questionsArray.push(
+            <div className="question-container" key={questionObject["id"]}>
+              <label className="question-label">{question}</label>
+              <input type="number" step="1" className="form-control" required />
+            </div>
+          );
+          break;
+        case "float":
+          // TODO add default value to each float
+          questionsArray.push(
+            <div className="question-container" key={questionObject["id"]}>
+              <label className="question-label">{question}</label>
+              <input
+                type="number"
+                step="0.1"
+                className="form-control"
+                required
+              />
+            </div>
+          );
+          break;
+        default:
+          // throw an error because there should be a selectionType on every question
+          // TODO create error handler component
+          throw Error;
+      }
+    }
+    setQuestions(questionsArray);
+  }, []);
+  return (
+    <div className="survey-form-container">
+      <div className="questions-container">{questions}</div>
+      <button id="submit-survey-button" onClick={() => props.submitSurvey()}>
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default SurveyForm;
--- src/pages/SurveyPage/SurveyPage.jsx
+++ src/pages/SurveyPage/SurveyPage.jsx
@@ -1,26 +1,54 @@
 import React, { useState, useEffect } from "react";
 import "./SurveyPage.css";
+import SurveyForm from "../../components/SurveyForm/SurveyForm";
 
 function SurveyPage() {
   const [description, setDescription] = useState([]);
   const split = window.location.pathname.split("/");
   const surveyName = split[2];
-  const survey = require(`../../data/surveys/${surveyName}.json`);
+  let survey;
+  try {
+    survey = require(`../../data/surveys/${surveyName}.json`);
+  } catch {
+    survey = "not-found";
+  }
+
+  // TODO implement this after backend
+  const submitSurvey = () => {
+    console.log("submitting survey...");
+  };
 
   useEffect(() => {
-    const descriptionSplit = survey.description.split("<br>");
-    const paragraphs = descriptionSplit.map((paragraph) => {
-      return <p>{paragraph}</p>;
-    });
-    setDescription(paragraphs);
-  }, [survey.description]);
+    if (survey !== "not-found") {
+      const descriptionSplit = survey.description.split("<br>");
+      const paragraphs = descriptionSplit.map((paragraph) => {
+        return <p>{paragraph}</p>;
+      });
+      setDescription(paragraphs);
+    }
+  }, [survey]);
 
-  return (
-    <div className="survey-page-container">
-      <h3>Welcome to the {survey.title} interactive webpage.</h3>
-      <div className="description-container">{description}</div>
-    </div>
-  );
+  if (survey === "not-found") {
+    return (
+      <div className="survey-page-container">
+        <h3>404. Not Found.</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="survey-page-container">
+        <h3>Welcome to the {survey.title} interactive webpage.</h3>
+        <div className="description-container">{description}</div>
+        <div className="survey-container">
+          <SurveyForm
+            questions={survey.questions}
+            key={survey.title}
+            submitSurvey={submitSurvey}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SurveyPage;
