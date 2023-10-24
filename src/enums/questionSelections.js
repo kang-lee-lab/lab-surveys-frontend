b4a10b75e24cad126bbf134e7649b756b0d01f5f
azhhng <azhhng@gmail.com>
2023-10-24 13:43:57 -0400

    change selections to have a corresponding answer id

--- src/enums/questionSelections.js
+++ src/enums/questionSelections.js
@@ -1,8 +1,8 @@
-export const AppliedSelections = Object.freeze([
-  "Did not apply to me at all",
-  "Applied to me to some degree, or some of the time",
-  "Applied to me to a considerable degree, or a good part of time",
-  "Applied to me very much, or most of the time",
-]);
+export const AppliedSelections = Object.freeze({
+  "Did not apply to me at all": 1,
+  "Applied to me to some degree, or some of the time": 2,
+  "Applied to me to a considerable degree, or a good part of time": 3,
+  "Applied to me very much, or most of the time": 4,
+});
 
-export const YesNoSelections = Object.freeze(["No", "Yes", "Uncertain"]);
+export const YesNoSelections = Object.freeze({ No: 1, Yes: 0, Uncertain: 2 });
