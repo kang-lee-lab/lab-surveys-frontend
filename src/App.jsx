05280151b7562fe206a0d49c9d24f2219baa106d
Annie <anntjx@gmail.com>
2023-11-04 21:24:46 -0400

    render demo survey from json

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import Header from "./components/Header/Header";
 import SurveyPage from "./pages/SurveyPage/SurveyPage";
-import Consent from "./pages/Consent/Consent";
 import DataColSurveys from "./pages/DataColSurveys/DataColSurveys";
-import DemoConsent from "./pages/DemoConsent/DemoConsent";
 import ResultsPage from "./pages/ResultsPage/ResultsPage";
 import Completed from "./pages/Completed/Completed";
-
+import GeneralConsent from "./pages/GeneralConsent/GeneralConsent";
+import Consent from "./pages/Consent/Consent";
 
 function App() {
   return (
@@ -16,12 +15,11 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/participate" element={<Consent />} />
+          <Route path="/participate" element={<GeneralConsent />} />
           <Route path="/survey/:name/results" element={<ResultsPage />} />
           <Route path="/survey/:name" element={<SurveyPage />} />
-            {/*<Route path="/survey/consent/:name" element={<Consent />} />*/}
-            <Route path={"/datasurveys"} element={<DataColSurveys/>} />
-            <Route path={"/survey/mangaconsent"} element={<DemoConsent />} />
+            <Route path="/survey/consent/:name" element={<Consent />} />
+            <Route path={"/data-surveys"} element={<DataColSurveys/>} />
             <Route path={"/survey/manga/completed"} element={<Completed />} />
         </Routes>
       </div>
