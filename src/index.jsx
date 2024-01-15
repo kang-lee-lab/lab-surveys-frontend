d7018ae124a428cf8f013a9c2e2329eda1ad2c2c
azhhng <azhhng@gmail.com>
2024-01-15 16:34:10 -0500

    wrap app with Auth0 provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="kang-lee-lab.us.auth0.com"
+      clientId="hxHYz2gN381quKZUHyUZBUFFreNRmaHS"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
